8f5eda9271fa6b308de2d013b328f10836444c80
Stefan Schwaha <the.stevee@gmail.com>
2022-03-21 20:36:43 +0100

    brush with random

--- public/Brush.js
+++ public/Brush.js
@@ -1,13 +1,17 @@
 class Fibre {
-    constructor() {
-        this.startX = 40;
-        this.startY = 60;
-        this.sizeStroke = 5;
+    constructor(brushStartX, brushStartY, brushStopX) {
+        this.startXNoise = 5;
+        this.startY = brushStartY;
+
+        this.sizeStroke = 1.5;
         this.curveTightness = 5;
         this.baseColor = "#ed4f3e";
-        this.colorNoise = 30;
+        this.colorNoise = 20;
+        this.brightnessNoise = 50;
 
-        this.colorFibre = distortColor(color(this.baseColor), this.colorNoise)
+        this.startX = brushStartX + getRandomFromInterval(-this.startXNoise, this.startXNoise);
+        this.stopX = brushStopX + getRandomFromInterval(-this.startXNoise, this.startXNoise);
+        this.colorFibre = brightenColor(distortColor(color(this.baseColor), this.colorNoise), this.brightnessNoise)
     }
 
     show(i) {
@@ -20,22 +24,25 @@ class Fibre {
         beginShape();
         curveVertex(this.startX, this.startY + this.sizeStroke * i);
         curveVertex(this.startX, this.startY + this.sizeStroke * i);
-        curveVertex(80, this.startY + this.sizeStroke * i);
-        curveVertex(160, this.startY + this.sizeStroke * i);
-        curveVertex(160, this.startY + this.sizeStroke * i);
+        // curveVertex(80, this.startY + this.sizeStroke * i);
+        curveVertex(this.stopX, this.startY + this.sizeStroke * i);
+        curveVertex(this.stopX, this.startY + this.sizeStroke * i);
         endShape();
     }
 }
 
 class Brush {
     constructor() {
+        this.brushStartX = 40;
+        this.brushStartY = 60;
+        this.brushStopX = 160;
+        // this.brushStartY = 60;
 
         this.fibresAmount = 30;
-
         this.fibres = []
 
         for (var i = 0; i < this.fibresAmount; i++) {
-            this.fibres.push(new Fibre());
+            this.fibres.push(new Fibre(this.brushStartX, this.brushStartY, this.brushStopX));
         }
 
     }
