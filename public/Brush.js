e61365694f049dc16e24622134cf10ac96e2825e
Stefan Schwaha <the.stevee@gmail.com>
2022-03-21 20:18:33 +0100

    brush basically works

--- public/Brush.js
+++ public/Brush.js
@@ -1,31 +1,48 @@
-class Brush {
+class Fibre {
     constructor() {
-
         this.startX = 40;
         this.startY = 60;
         this.sizeStroke = 5;
-        this.linesAmount = 30;
         this.curveTightness = 5;
         this.baseColor = "#ed4f3e";
         this.colorNoise = 30;
 
+        this.colorFibre = distortColor(color(this.baseColor), this.colorNoise)
+    }
+
+    show(i) {
+
+        noFill();
+        curveTightness(this.curveTightness);
+        stroke(this.colorFibre);
+        strokeWeight(this.sizeStroke);
+
+        beginShape();
+        curveVertex(this.startX, this.startY + this.sizeStroke * i);
+        curveVertex(this.startX, this.startY + this.sizeStroke * i);
+        curveVertex(80, this.startY + this.sizeStroke * i);
+        curveVertex(160, this.startY + this.sizeStroke * i);
+        curveVertex(160, this.startY + this.sizeStroke * i);
+        endShape();
+    }
+}
+
+class Brush {
+    constructor() {
+
+        this.fibresAmount = 30;
+
+        this.fibres = []
+
+        for (var i = 0; i < this.fibresAmount; i++) {
+            this.fibres.push(new Fibre());
+        }
+
     }
 
     show() {
-        noFill();
-        curveTightness(this.curveTightness);
-
-        for (var i = 0; i < this.linesAmount; i++) {
-
-            stroke(distortColor(color(this.baseColor), this.colorNoise));
-            strokeWeight(this.sizeStroke);
-            beginShape();
-            curveVertex(this.startX, this.startY + this.sizeStroke * i);
-            curveVertex(this.startX, this.startY + this.sizeStroke * i);
-            curveVertex(80, this.startY + this.sizeStroke * i);
-            curveVertex(160, this.startY + this.sizeStroke * i);
-            curveVertex(160, this.startY + this.sizeStroke * i);
-            endShape();
+        for (var i = 0; i < this.fibres.length; i++) {
+            this.fibres[i].show(i);
         }
     }
 }
