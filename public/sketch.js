20a4b9d6f7e01a524b9bdaf354f8e3df1f630e35
Stefan Schwaha <the.stevee@gmail.com>
2022-05-02 07:03:59 +0200

    test inbetween

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,7 @@
 // trace, debug, info, warn, error
 // const SWITCH_LOGGING_LEVEL = "warn";
-// const SWITCH_LOGGING_LEVEL = "info";
-const SWITCH_LOGGING_LEVEL = "debug";
+const SWITCH_LOGGING_LEVEL = "info";
+// const SWITCH_LOGGING_LEVEL = "debug";
 
 const CANVAS_WIDTH = 1080;
 const CANVAS_HEIGHT = CANVAS_WIDTH;
@@ -163,13 +163,13 @@ function setup() {
   // grainy_gradient = Pattern.create_grainy_gradient(width, height);
 
   // ENDRESULT
-  // canvasOverlay = Pattern.create_canvas(width, height);
+  canvasOverlay = Pattern.create_canvas(width, height);
 
   // ENDRESULT
-  // splatter = Pattern.create_splatter_splitter(width, height);
+  splatter = Pattern.create_splatter_splitter(width, height);
 
   // ENDRESULT
-  // canvasAgent = new DumbAgent();
+  canvasAgent = new DumbAgent();
 
   agentPaintbrushData = {
     customWidth: width,
@@ -187,7 +187,7 @@ function setup() {
   }
 
   // ENDRESULT
-  // agentPaintbrush = new DumbAgent(agentPaintbrushData);
+  agentPaintbrush = new DumbAgent(agentPaintbrushData);
 
   sphereData = {
     custom_width: 400,
@@ -239,7 +239,7 @@ function setup() {
   }
 
   // ENDRESULT
-  // backgroundSphere = new paintedSphere(sphereBackgroundData);
+  backgroundSphere = new paintedSphere(sphereBackgroundData);
 
   // paper = paper.get()
   // paper.mask(noise_fog);
@@ -265,8 +265,9 @@ function setup() {
     padding: 50,
   }
 
-  // frontGrid = new IntersectGrid(frontGridData);
-  // backGrid = new IntersectGrid(backGridData);
+  // END RESULT
+  frontGrid = new IntersectGrid(frontGridData);
+  backGrid = new IntersectGrid(backGridData);
 
   // exampleGrid = new IntersectGrid();
 
@@ -304,9 +305,9 @@ function draw() {
   // image(paper, - width / 2, - height / 2, paper.width * SCALING_FACTOR, paper.height * SCALING_FACTOR);
 
   // ENDRESULT
-  // image(backgroundSphere.buffer, - width / 2, - height / 2, backgroundSphere.buffer.width * SCALING_FACTOR, backgroundSphere.buffer.height * SCALING_FACTOR);
+  image(backgroundSphere.buffer, - width / 2, - height / 2, backgroundSphere.buffer.width * SCALING_FACTOR, backgroundSphere.buffer.height * SCALING_FACTOR);
   // ENDRESULT
-  // image(splatter, - width / 2, - height / 2, splatter.width * SCALING_FACTOR, splatter.height * SCALING_FACTOR);
+  image(splatter, - width / 2, - height / 2, splatter.width * SCALING_FACTOR, splatter.height * SCALING_FACTOR);
 
   // maska
   // brush.buffer = brush.buffer.get();
@@ -354,16 +355,16 @@ function draw() {
 
   // ENDRESULT
   // CANVAS
-  // image(canvasOverlay, - width / 2, - height / 2, canvasOverlay.width * SCALING_FACTOR, canvasOverlay.height * SCALING_FACTOR);
+  image(canvasOverlay, - width / 2, - height / 2, canvasOverlay.width * SCALING_FACTOR, canvasOverlay.height * SCALING_FACTOR);
 
   // ENDRESULT
-  // backGrid.show();
-  // frontGrid.show();
+  backGrid.show();
+  frontGrid.show();
 
   // exampleGrid.show();
 
   // ENDRESULT
-  // image(canvasAgent.buffer, - width / 2, - height / 2, canvasAgent.buffer.width * SCALING_FACTOR, canvasAgent.buffer.height * SCALING_FACTOR);
+  image(canvasAgent.buffer, - width / 2, - height / 2, canvasAgent.buffer.width * SCALING_FACTOR, canvasAgent.buffer.height * SCALING_FACTOR);
 
 
   // PERLIN Noise
@@ -373,7 +374,7 @@ function draw() {
   // linesExample.show()
 
   // ENDRESULT
-  // image(agentPaintbrush.buffer, (agentPaintbrush.posXImage - width / 2) * SCALING_FACTOR, (agentPaintbrush.posYImage - height / 2) * SCALING_FACTOR, agentPaintbrush.buffer.width * SCALING_FACTOR, agentPaintbrush.buffer.height * SCALING_FACTOR);
+  image(agentPaintbrush.buffer, (agentPaintbrush.posXImage - width / 2) * SCALING_FACTOR, (agentPaintbrush.posYImage - height / 2) * SCALING_FACTOR, agentPaintbrush.buffer.width * SCALING_FACTOR, agentPaintbrush.buffer.height * SCALING_FACTOR);
 
   // EXAMPLE PaintAreas
   // examplePaintBrushArea.show();
