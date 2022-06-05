257235530fc6002c96c9d96f9eb3c1e0468e0f91
Stefan Schwaha <the.stevee@gmail.com>
2022-06-05 19:41:11 +0200

    stammersdorf cut

--- public/utils.js
+++ public/utils.js
@@ -94,8 +94,8 @@ function scaleDynamically() {
         exportRatio = dynamicHeightRatio;
     }
 
-    rescaling_width = exportPaper.width / exportRatio.toFixed(2);
-    rescaling_height = exportPaper.height / exportRatio.toFixed(2);
+    rescaling_width = exportPaper.width / exportRatio;
+    rescaling_height = exportPaper.height / exportRatio;
 }
 
 // each time window.innerWidth changes
